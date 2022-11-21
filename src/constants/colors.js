edb6604b76ccc18238e4c279c2da2b18124b5351
hscioscia <hernan.scioscia@gmail.com>
2022-11-21 18:24:55 -0300

    Falta funcionalidad botones screen start-game

---
+++ src/constants/colors.js
@@ -0,0 +1,10 @@
+export default {
+    primary: '#272932',
+    secondary: '#4d7ea8',
+    text: '#212121',
+    background: '#b6c2d9',
+    white: '#FFFFFF',
+    black: '#000000',
+    gray: '#c9ffe2',
+    back: '#e5625e'
+}
